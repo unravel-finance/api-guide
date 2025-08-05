8260dd08ca3a66a969964405cb24ad9a0ad0c3d2
Daniel Szemerey <account@szemerey.eu>
2025-08-05 13:20:45 +0200

    f

--- factors/api.py
+++ factors/api.py
@@ -4,9 +4,13 @@ import requests
 BASEAPI = "https://unravel.finance/api/v1"
 
 
-def get_portfolio_factors_historical(portfolioId: str, API_KEY: str) -> pd.Series:
+def get_portfolio_factors_historical(
+    portfolioId: str,
+    tickers: list[str],
+    API_KEY: str,
+) -> pd.Series:
     url = f"{BASEAPI}/portfolio/factors"
-    params = {"id": portfolioId, "tickers": "BTC,ETH"}
+    params = {"id": portfolioId, "tickers": ",".join(tickers)}
     headers = {"X-API-KEY": API_KEY}
     response = requests.get(url, headers=headers, params=params)
     assert (
@@ -51,3 +55,26 @@ def get_price_series(
     return pd.Series(response["data"], index=pd.to_datetime(response["index"])).rename(
         ticker
     )
+
+
+def get_tickers(portfolioId: str, API_KEY: str) -> list[str]:
+    """
+    Fetch the tickers for a portfolio from the Unravel API.
+
+    Args:
+        portfolioId (str): The portfolio ID
+        API_KEY (str): The API key to use for the request
+
+    Returns:
+        pd.Series: Time series of the risk signal with datetime index
+    """
+    url = f"{BASEAPI}/portfolio/tickers"
+    params = {"id": portfolioId, "universe_size": "20"}
+    headers = {"X-API-KEY": API_KEY}
+    response = requests.get(url, headers=headers, params=params)
+    assert (
+        response.status_code == 200
+    ), f"Error fetching price series for {portfolioId}, response: {response.json()}"
+
+    response = response.json()
+    return response["tickers"]
--- factors/main.py
+++ factors/main.py
@@ -1,13 +1,18 @@
+# %%
 import os
 
 import pandas as pd
 
-from .alphalens import simplified_factor_analysis
-from .api import get_portfolio_factors_historical, get_price_series
+from factors.alphalens import simplified_factor_analysis
+from factors.api import get_portfolio_factors_historical, get_price_series, get_tickers
 
 UNRAVEL_API_KEY = os.environ.get("UNRAVEL_API_KEY")
 portfolio = "momentum_enhanced"
-historical_factors = get_portfolio_factors_historical(portfolio, UNRAVEL_API_KEY)
+available_tickers = get_tickers(portfolio, UNRAVEL_API_KEY)
+historical_factors = get_portfolio_factors_historical(
+    portfolio, available_tickers, UNRAVEL_API_KEY
+)
+
 underlying = pd.DataFrame(
     {
         underlying: get_price_series(underlying, UNRAVEL_API_KEY)
@@ -17,3 +22,5 @@ underlying = pd.DataFrame(
 
 
 simplified_factor_analysis(historical_factors, underlying)
+
+# %%
