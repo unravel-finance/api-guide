cd93f62de0092f4276aa4825f7293d1002ad459a
Daniel Szemerey <account@szemerey.eu>
2025-09-15 17:40:57 +0200

    f

--- get_live_weights.py
+++ get_live_weights.py
@@ -4,7 +4,7 @@ from unravel_client import get_live_weights
 from analysis.utils import get_env
 
 UNRAVEL_API_KEY = get_env("UNRAVEL_API_KEY")
-portfolio = "beta.5"
+portfolio = "momentum_enhanced.20"
 
 live_weights = get_live_weights(portfolio, UNRAVEL_API_KEY)
 print(live_weights)
